511ececcd9510651ed7ca6ff16feff4a90ea7d38
Reese Sorrell <joreesesorrell@gmail.com>
2023-05-15 19:16:28 -0500

    Add clarifying instructions to grey screen

--- src/components/GreyScreen.js
+++ src/components/GreyScreen.js
@@ -7,7 +7,12 @@ const hideGreyScreen = (e) => {
 export default function GreyScreen() {
     return (
         <div className="GreyScreen" onClick={hideGreyScreen}>
-            Click to begin!
+            A note will be choosen at the top for you to find.
+            <br />To hear each note hover over the box with your cursor.
+            <br />Notes are shuffled between each round.
+            <br />*Make sure your computer volume is on*
+            <br />(Check your browser sound settings if not playing)
+            <br />Click to begin!
         </div>
     )
 }
