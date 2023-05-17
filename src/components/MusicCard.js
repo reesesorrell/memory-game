d6215ba5456419af6e9786f3d1a4550f87bbf570
Reese Sorrell <joreesesorrell@gmail.com>
2023-05-17 10:02:43 -0500

    End sounds when mouse outside of box

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -3,13 +3,18 @@ import "../styles/MusicCard.css"
 
 export default function MusicCard({note, onNoteClick, cardsFlipped}) {
     let highOrLow = Math.round(Math.random()) + 2;
+    let audioObject = new Audio(require("../music-notes/" + note + highOrLow + ".mp3"))
     function play() {
-        new Audio(require("../music-notes/" + note + highOrLow + ".mp3")).play();
+        audioObject.play();
+    }
+    function stopPlay() {
+      audioObject.pause();
+      audioObject.currentTime = 0;
     }
     return (
         <div className="scene scene--card">
         <div className={`card ${cardsFlipped ? "is-flipped" : ''}`}>
-          <div className="card__face card__face--front " id={note} onMouseOver={play} onClick={onNoteClick} muted="muted"></div>
+          <div className="card__face card__face--front " id={note} onMouseOver={play} onMouseOut={stopPlay} onClick={onNoteClick} muted="muted"></div>
           <div className="card__face card__face--back">{note}</div>
         </div>
       </div>
